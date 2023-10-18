6132301bbd8640428eef00fcfe86a738a81bac0d
sudoallenlopes23 <allenlopes10@gmail.com>
2023-10-18 20:01:34 +0530

    Converted the unicode values in the first row i.e numbers from 1 to 26 to it's equivalent characters

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ for (let i = 0; i < rows; i++) { // here we have set i = 0, 0 < 100,
 }
 
 for(let i = 0; i < cols; i++) {
-    let addressRow = document.createElement("div"); // it will create a new div for every new row no. added
-    addressRow.setAttribute("class", "address-row"); // this line will work as class name which will add styling i.e the border to the row nums.
-    addressRow.innerText = i + 1; // since i= 0, it will start from i+1 i.e 0+1 which is 1
-    addressRowCont.appendChild(addressRow);
+  let addressRow = document.createElement("div"); // it will create a new div for every new row no. added
+  addressRow.setAttribute("class", "address-row"); // this line will work as class name which will add styling i.e the border to the row nums.
+  addressRow.innerText = String.fromCharCode(65 + i); // The String.fromCharCode() method converts Unicode values to characters. So here Unicode value which starts from 1 so character 1st is A so, we have defined (65 + i) so that in each new column box it increments to next character, like 65 + 0 = 65, so A then i++ so now i = 1, so 65 + 1 = 66, so we have B, so this goes on upto 26th character i.e Z.
+  addressRowCont.appendChild(addressRow);
 }
