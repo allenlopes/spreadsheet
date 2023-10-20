d80483ce1d84b8f08bd140f31dcce02f7df6206f
sudoallenlopes23 <allenlopes10@gmail.com>
2023-10-21 02:14:26 +0530

    Added script for cells inside the grid

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ let cols = 26; // there are total 26 alphabets
 // for column numbers ->
 let addressColCont = document.querySelector(".address-col-cont");
 
+
 // for row container alphabets ->
 let addressRowCont = document.querySelector(".address-row-cont");
+
+
+// for the grid's inside cells ->
+let cellsCont = document.querySelector(".cells-cont");
+
 
 for (let i = 0; i < rows; i++) { // here we have set i = 0, 0 < 100,
     let addressCol = document.createElement("div"); // it will create a new div for every new row no. added
@@ -21,4 +27,20 @@ for(let i = 0; i < cols; i++) {
   addressRow.setAttribute("class", "address-row"); // this line will work as class name which will add styling i.e the border to the row nums.
   addressRow.innerText = String.fromCharCode(65 + i); // The String.fromCharCode() method converts Unicode values to characters. So here Unicode value which starts from 1 so character 1st is A so, we have defined (65 + i) so that in each new column box it increments to next character, like 65 + 0 = 65, so A then i++ so now i = 1, so 65 + 1 = 66, so we have B, so this goes on upto 26th character i.e Z.
   addressRowCont.appendChild(addressRow);
+}
+
+
+// This nested for loop is for the grid's inside cells, in which we are going to type the values.
+/* Here first what happens is, for i's 1st iteration of for loop row is created, like suppose for row no. 1 which will have 1 to 26 cells,
+and then it goes to j's 1st iteration of for loop where columns blocks are created from 1 to 26 cells, then it goes to iteration no. 2, and likewise
+the same goes on until the last row i.e 100th row is reached and finished and i's for loop is completed. */
+for(let i = 0; i < rows; i++){  //this is outer for loop for row, which will create new cells from 1 to 100.
+  let rowCont = document.createElement("div"); // it will create a new row element.
+  rowCont.setAttribute("class", "row-cont"); // it is for styling the row i.e the border to the row cells in the style.css.
+  for (let j = 0; j < cols; j++) { // this is inner for loop for columns, which will create new rows from 1 to 26.
+    let cell = document.createElement("div"); // it will add 26 cells inside the rows.
+    cell.setAttribute("class", "cell"); // it is for styling the columns i.e the border to the column cells in the style.css
+    rowCont.appendChild(cell); // it will append the 26 cells inside the created row Container
+  }
+  cellsCont.appendChild(rowCont); // it will append the cells created iteration by iteration inside our cells-cont which we have created in index.html
 }
