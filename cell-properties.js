8303f81f6ddb3333023bcc391cdf40d69902ccbf
sudoallenlopes23 <allenlopes10@gmail.com>
2023-10-31 23:37:10 +0530

    In cell-properties.js, added some cells default properties

--- cell-properties.js
+++ cell-properties.js
@@ -8,7 +8,14 @@ for (let i = 0; i < rows; i++) {
     let sheetRow = [];
     for(let j = 0; j < cols; j++){
         let cellProp = {
-
+            bold: false,
+            italic: false,
+            underline: false,
+            alignment: "left",
+            fontFamily: "monospace",
+            fontSize: "14",
+            fontColor: "#000000",
+            BGcolor: "#000000", // Just for indication purpose
         }
         sheetRow.push(cellProp);
     }
