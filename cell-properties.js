89d4eeac16f04c638001d7eb0632dcd4adfffca4
sudoallenlopes23 <allenlopes10@gmail.com>
2023-11-14 23:30:25 +0530

    In cell-properties.js, added a empty value, to store the cell's value

--- cell-properties.js
+++ cell-properties.js
@@ -16,6 +16,7 @@ for (let i = 0; i < rows; i++) {
             fontSize: "14",
             fontColor: "#000000",
             BGcolor: "#000000", // Just for indication purpose
+            value: "", // Cell ki jo bhi value hogi isme store hogi
         }
         sheetRow.push(cellProp);
     }
