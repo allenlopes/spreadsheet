c7d8aae895086a3449eb1ea539dd1196ec526b1e
sudoallenlopes23 <allenlopes10@gmail.com>
2023-11-15 00:03:01 +0530

    In formula.js, cell mai jo value dalenge woh ek empty variable 'value' mai store hogi jo hame baadmein use karneko aasan hogi

--- formula.js
+++ formula.js
@@ -1,13 +1,13 @@
 for (let i = 0; i < rows; i++) {
     for(let j = 0; j < cols; j++){
         let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
-        cell.addEventListener('blur', (e) => { // I have took the 'blur' eventlistener, there are two event listener of this kind, one is 'blur' and another is 'focused'(If more info. is required, google it)
+        cell.addEventListener("blur", (e) => { // I have took the 'blur' eventlistener, there are two event listener of this kind, one is 'blur' and another is 'focused'(If more info. is required, google it)
              let address = addressBar.value; // Whenever we click on different cell, it will take the value inside address of the cell which we were on before clicking on to different cell.
-             let [activecell, cellProp] = activecell(address); // After getting the address, we will access he object inside the cell.
-             let enteredData = activecell.innerText; // whatever the modified data is done by user, we take it in 'enteredData'.
+             let [activeCell, cellProp] = activecell(address); // After getting the address, we will access he object inside the cell.
+             let enteredData = activeCell.innerText; // whatever the modified data is done by user, we take it in 'enteredData'.
 
              cellProp.value = enteredData; // cell ki object mai value store hogi jo bhi ham cell mai dalenge
-             console.log(cellProp);
+             // console.log(cellProp);
         })
     }
 }
