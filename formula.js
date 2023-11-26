67e1f9d318b732fa5299d289d153380d53a7eee9
sudoallenlopes23 <allenlopes10@gmail.com>
2023-11-26 23:03:16 +0530

    In formula.js made a function named addChildToParent().

--- formula.js
+++ formula.js
@@ -25,6 +25,11 @@ formulaBar.addEventListener("keydown", (e) => {
     }
 })
 
+
+function addChildToParent() {
+
+}
+
 function evaluateFormula(formula) {
     let encodedFormula = formula.split(" "); // formula must be space seperated
     for( let i = 0; i < encodedFormula.length; i++){
