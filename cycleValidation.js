ce45a49ef45c4800bced3deaed11f2ac1602c5bc
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-04 17:55:00 +0530

    In cycleValidation.js, changes..

---
+++ cycleValidation.js
@@ -0,0 +1,10 @@
+// Storage
+let graphComponentMatrix = [];
+
+for (let i = 0; i < rows; i++){
+    let row = [];
+    for(let j = 0; j < cols; j++){
+
+    }
+    graphComponentMatrix.push(row);
+}
