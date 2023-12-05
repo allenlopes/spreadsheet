380162f7c1a6749aae6b68ede4b3d6e00c2be486
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-05 18:58:12 +0530

    In cycleValidation.js,...

--- cycleValidation.js
+++ cycleValidation.js
@@ -1,9 +1,11 @@
-// Storage
+// Storage -> 2D matrix (Basic needed)
 let graphComponentMatrix = [];
 
 for (let i = 0; i < rows; i++){
     let row = [];
     for(let j = 0; j < cols; j++){
+        // Why array? ->More than 1 child relation(dependency)
+        row.push([]);
 
     }
     graphComponentMatrix.push(row);
