5104e5836f8f726c8416327483ad05204c7741c8
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-07 23:45:21 +0530

    In formula.js, some edits in function addChildToGraphComponent()

--- formula.js
+++ formula.js
@@ -47,6 +47,14 @@ formulaBar.addEventListener("keydown", (e) => {
 
 function addChildToGraphComponent(formula, childAddress) {
     let [crid, ccid] =  decodeRIDCIDFromAddress(childAddress);
+
+    let encodedFormula = formula.split(" ");
+    for (let i = 0; i < encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if(asciiValue >= 65 && asciiValue <= 90) {
+            let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula[i]);
+        }
+    }
 }
 
 
