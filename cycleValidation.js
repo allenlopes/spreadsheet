6db4a99802786162a5c1550a16bb13b4c751e9aa
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-13 18:18:38 +0530

    In cycleValidation.js, edits in function isGraphCyclic().

--- cycleValidation.js
+++ cycleValidation.js
@@ -1,4 +1,4 @@
-// Storage -> 2D matrix (Basic needed)
+// Storage -> 2D array (Basic needed)
 let graphComponentMatrix = [];
 
 for (let i = 0; i < rows; i++){
@@ -10,7 +10,16 @@ for (let i = 0; i < rows; i++){
     graphComponentMatrix.push(row);
 }
 
-// True -> Cycle, False -> Not Cyclic
+// True -> Cyclic, False -> Not Cyclic
 function isGraphCyclic() {
+    // Dependency -> Visited, dfsVisited (2D array)
+    let visited = [];
+    let dfsVisited = [];
 
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++){
+            visited.push([]);
+            dfsVisited.push([]);
+        }
+    }
 }
