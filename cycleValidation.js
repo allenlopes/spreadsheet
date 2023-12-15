d9fb1b7d1e4d2deb9efefb8edd99e7cd64f9e470
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-15 16:26:10 +0530

    In cycleValidation.js, edits in function dfsCycleDetection().

--- cycleValidation.js
+++ cycleValidation.js
@@ -37,9 +37,20 @@ function isGraphCyclic(graphComponentMatrix) {
 // End -> dfsVisited(False)
 // If visited[i][j] -> True, i.e already explored path, then go back, since no use to explore again
 // Cycle Detection condition -> if (visited[i][j] == true && dfsVisited[i][j] == true) -> cycle
+// Return -> True/False
+// True -> Cyclic, False -> Not Cyclic
 function dfsCycleDetection(graphComponentMatrix, srcr, srcc, visited, dfsVisited) { // srcr -> source row, srcc -> source column
     visited[srcr][srcc] = true;
     dfsVisited[srcr][srcc] = true;
+
+    // A1 -> [ [0, 1], [1, 0], [5, 10], ..... ]
+    for (let children = 0; children < graphComponentMatrix[srcr][srcc].length; children++) {
+        let [crid, ccid] = graphComponentMatrix[srcr][srcc][children]; // crid -> child row id, ccid -> child column id
+        if (visited[crid][ccid] = false) {
+            let response = dfsCycleDetection(graphComponentMatrix, crid, ccid, visited, dfsVisited);
+            if (response == true) return true;
+        }
+    }
 
     dfsVisited[srcr][srcc] = false;
 }
