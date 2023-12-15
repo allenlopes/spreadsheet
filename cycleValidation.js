94ded72364cbaa1fb17c2409db2be629d68f430c
sudoallenlopes23 <allenlopes10@gmail.com>
2023-12-15 16:16:15 +0530

    In cycleValidation.js, edits in function dfsCycleDetection().

--- cycleValidation.js
+++ cycleValidation.js
@@ -13,8 +13,8 @@ for (let i = 0; i < rows; i++){
 // True -> Cyclic, False -> Not Cyclic
 function isGraphCyclic(graphComponentMatrix) {
     // Dependency -> Visited, dfsVisited (2D array)
-    let visited = [];
-    let dfsVisited = [];
+    let visited = []; // Node Visited trace
+    let dfsVisited = []; // Stack visited trace
 
     for(let i = 0; i < rows; i++) {
         let visitedRow = [];
@@ -27,11 +27,19 @@ function isGraphCyclic(graphComponentMatrix) {
 
     for (let i = 0; i < rows; i++){
         for(let j = 0; j < cols; j++){
-            dfsCycleDetection(graphComponentMatrix);
+            dfsCycleDetection(graphComponentMatrix, i, j, visited, dfsVisited);
         }
     }
 }
 
 
-function dfsCycleDetection() {
+// Start -> visited(True) then, dfsVisited(True)
+// End -> dfsVisited(False)
+// If visited[i][j] -> True, i.e already explored path, then go back, since no use to explore again
+// Cycle Detection condition -> if (visited[i][j] == true && dfsVisited[i][j] == true) -> cycle
+function dfsCycleDetection(graphComponentMatrix, srcr, srcc, visited, dfsVisited) { // srcr -> source row, srcc -> source column
+    visited[srcr][srcc] = true;
+    dfsVisited[srcr][srcc] = true;
+
+    dfsVisited[srcr][srcc] = false;
 }
